aab9651f3fd8b180616b8e9da5c4acb3f333c11c
Max Jando <max.jando@googlemail.com>
2013-06-20 22:08:46 +0200

    ADS PÜ5 Aufgabe 1 Teil 1 bearbeitet

--- ADS/Übung 5/Teil1/Liste/Playlist.java
+++ ADS/Übung 5/Teil1/Liste/Playlist.java
@@ -9,258 +9,272 @@ import java.io.*;
 
 public class Playlist {
 
-	/**
-	 * Erstelle Queue basierend auf einer Liste
-	 * 
-	 */
-	String name = "";
-	PlaylistElement root;
+    /**
+     * Erstelle Queue basierend auf einer Liste
+     */
+    String name = "";
+    PlaylistElement root;
 
-	public Playlist() {
-		root = null;
-	}
+    public Playlist() {
+        root = null;
+    }
 
-	/**
-	 * Erstelle Queue basierend auf einer Liste
-	 * 
-	 * @param name
-	 *            Der Name der Playlist
-	 */
-	public Playlist(String name) {
-		root = null;
-		this.name = name;
-	}
+    /**
+     * Erstelle Queue basierend auf einer Liste
+     *
+     * @param name Der Name der Playlist
+     */
+    public Playlist(String name) {
+        root = null;
+        this.name = name;
+    }
 
-	/**
-	 * Emulation einer Play-Funktionalität: Erstes Element wird gelöscht
-	 */
-	public void play() {
-		if (root != null) {
-			root = root.nextElement;
-		}
-	}
+    /**
+     * Emulation einer Play-FunktionalitÃ¤t: Erstes Element wird gelÃ¶scht
+     */
+    public void play() {
+        if (root != null) {
+            root = root.nextElement;
+        }
+    }
 
-	/**
-	 * †bergebener Musiktitel wird am Ende der Playlist hinzugefügt
-	 * 
-	 * @param neuerMusiktitel
-	 *            Der Musiktitel, der der Playlist hinzugefügt werden soll.
-	 */
-	public void hinzufuegenAmEnde(Musiktitel neuerMusiktitel) {
-		if (root != null) {
-			PlaylistElement temp = root;
-			while (temp.nextElement != null) {
-				temp = temp.nextElement;
-			}
-			temp.nextElement = new PlaylistElement(neuerMusiktitel, null);
-		} else {
-			root = new PlaylistElement(neuerMusiktitel, null);
-		}
+    /**
+     * Ãœbergebener Musiktitel wird am Ende der Playlist hinzugefÃ¼gt
+     *
+     * @param neuerMusiktitel Der Musiktitel, der der Playlist hinzugefÃ¼gt werden soll.
+     */
+    public void hinzufuegenAmEnde(Musiktitel neuerMusiktitel) {
+        if (root != null) {
+            PlaylistElement temp = root;
+            //Schleife durchlÃ¤uft die Listen-Elemente(Musikttitel), solgange bis das Ende erreicht ist
+            while (temp.nextElement != null) {
+                temp = temp.nextElement;
+            }
+            temp.nextElement = new PlaylistElement(neuerMusiktitel, null);
+        } else {
+            root = new PlaylistElement(neuerMusiktitel, null);
+        }
 
-	}
+    }
 
-	/**
-	 * Übergebener Musiktitel wird aus Playlist entfernt.
-	 * 
-	 * @param zuEntfernen
-	 *            Der Musiktitel, der aus der Playlist entfernt werden soll.
-	 */
-	public void entferneMusiktitel(Musiktitel zuEntfernen) {
-		PlaylistElement temp = root;
-		PlaylistElement prev = root;
-		if (root.value == zuEntfernen) {
-			root = null;
-		} else {
-			while (temp.value != zuEntfernen) {
-				prev = temp;
-				temp = temp.nextElement;
-			}
-			prev.nextElement = temp.nextElement;
-		}
-	}
+    /**
+     * ï¿½bergebener Musiktitel wird aus Playlist entfernt.
+     *
+     * @param zuEntfernen Der Musiktitel, der aus der Playlist entfernt werden soll.
+     */
+    public void entferneMusiktitel(Musiktitel zuEntfernen) {
+        PlaylistElement temp = root;
+        PlaylistElement prev = root;
+        if (root.value == zuEntfernen) {
+            root = null;
+        } else {
+            //Schleife durchlÃ¤uft die Listen-Elemente(Musikttitel), solganege bis das zu lÃ¶schende Element gefunden wurde
+            while (temp.value != zuEntfernen) {
+                prev = temp;
+                temp = temp.nextElement;
+            }
+            prev.nextElement = temp.nextElement;
+        }
+    }
 
-	/**
-	 * Alle in dieser Playlist enthaltenen Titel als Array zurückliefern
-	 * 
-	 * @return Ein Array, das so lang ist, wie die Playlist Titel enthält.
-	 */
-	public Musiktitel[] getMusiktitel() {
-		Musiktitel[] musikListe;
-		PlaylistElement temp = root;
-		if (root == null) {
-			return null;
-		}
-		int counter = 0;
-		while (temp != null) {
-			temp = temp.nextElement;
-			counter++;
-		}
-		temp = root;
-		musikListe = new Musiktitel[counter];
-		musikListe[0] = temp.value;
-		int musiktitel = 0;
+    /**
+     * Alle in dieser Playlist enthaltenen Titel als Array zurï¿½ckliefern
+     *
+     * @return Ein Array, das so lang ist, wie die Playlist Titel enthï¿½lt.
+     */
+    public Musiktitel[] getMusiktitel() {
+        Musiktitel[] musikListe;
+        PlaylistElement temp = root;
+        if (root == null) {
+            return null;
+        }
+        int counter = 0;
+        //Schleife zÃ¤hlt die EintrÃ¤ge in der Liste
+        while (temp != null) {
+            temp = temp.nextElement;
+            counter++;
+        }
+        temp = root;
+        musikListe = new Musiktitel[counter];
+        musikListe[0] = temp.value;
+        int musiktitel = 0;
 
-		while (temp != null) {
-			musikListe[musiktitel] = temp.value;
-			temp = temp.nextElement;
-			musiktitel++;
-		}
-		return musikListe;
-	}
+        //Schleife durchlÃ¤uft die Liste und fÃ¼llt ein Array
+        while (temp != null) {
+            musikListe[musiktitel] = temp.value;
+            temp = temp.nextElement;
+            musiktitel++;
+        }
+        return musikListe;
+    }
 
-	/**
-	 * Alle Inhalte der Playlist löschen.
-	 */
-	public void leerePlaylist() {
-		root = null;
-	}
+    /**
+     * Alle Inhalte der Playlist lÃ¶schen.
+     */
+    public void leerePlaylist() {
+        root = null;
+    }
 
-	/**
-	 * Übergebene Datei wird zeilenweise eingelesen. Die Datei muss
-	 * folgendermaßen aufgebaut sein: Titel [Tab] Künstler [neue Zeile]
-	 * 
-	 * @param file
-	 * @throws IOException
-	 */
-	public void leseAusDatei(File file) throws IOException {
+    /**
+     * Ãœbergebene Datei wird zeilenweise eingelesen. Die Datei muss
+     * folgendermaÃŸen aufgebaut sein: Titel [Tab] KÃ¼nstler [neue Zeile]
+     *
+     * @param file
+     * @throws IOException
+     */
+    public void leseAusDatei(File file) throws IOException {
 
-		int stelleTabulator = 0;
-		String titel = "";
-		String kuenstler = "";
-		FileReader eingabestrom = new FileReader(file);
-		BufferedReader eingabe = new BufferedReader(eingabestrom);
-		String eingabeZeile = eingabe.readLine();
-		// Verarbeitungsschleife, solange nicht EOF
-		while (eingabeZeile != null) {
-			// Verarbeitung von eingabeZeile genau wie bisher über
-			// die Tastatur (ggf. StringTokenizer, ...parse..() etc.)
-			stelleTabulator = eingabeZeile.lastIndexOf("\t".toString());
-			for (int buchstabeTitel = 0; buchstabeTitel < stelleTabulator; buchstabeTitel++) {
-				titel += String.valueOf(eingabeZeile.charAt(buchstabeTitel));
-			}
-			for (int buchstabeKuenstler = stelleTabulator + 1; buchstabeKuenstler < eingabeZeile
-					.length(); buchstabeKuenstler++) {
-				kuenstler += String.valueOf(eingabeZeile
-						.charAt(buchstabeKuenstler));
-			}
-			hinzufuegenAmEnde(new Musiktitel(kuenstler, titel));
-			kuenstler = "";
-			titel = "";
-			eingabeZeile = eingabe.readLine();
-		}
-		eingabe.close(); // Datei schließen
+        int stelleTabulator = 0;
+        String titel = "";
+        String kuenstler = "";
+        FileReader eingabestrom = new FileReader(file);
+        BufferedReader eingabe = new BufferedReader(eingabestrom);
+        String eingabeZeile = eingabe.readLine();
+        // Verarbeitungsschleife, solange nicht EOF
+        while (eingabeZeile != null) {
+            //Lese Tabulatorstelle ein
+            stelleTabulator = eingabeZeile.lastIndexOf("\t".toString());
+            //Lese bis zum Tabulator ein --> Titel
+            for (int buchstabeTitel = 0; buchstabeTitel < stelleTabulator; buchstabeTitel++) {
+                //Buchstabe fÃ¼r Buchstabe wird der String gebaut
+                titel += String.valueOf(eingabeZeile.charAt(buchstabeTitel));
+            }
+            //Lese alles nach dem Tabulator ein --> KÃ¼nstler
+            for (int buchstabeKuenstler = stelleTabulator + 1; buchstabeKuenstler < eingabeZeile
+                    .length(); buchstabeKuenstler++) {
+                //Buchstabe fÃ¼r Buchstabe wird der String gebaut
+                kuenstler += String.valueOf(eingabeZeile
+                        .charAt(buchstabeKuenstler));
+            }
+            //Erstelle einen Descriptor auf ein neues Musiktitel-Objekt, welches dann hinten an die Queue(Liste) angefÃ¼gt wird
+            hinzufuegenAmEnde(new Musiktitel(kuenstler, titel));
+            kuenstler = "";
+            titel = "";
+            eingabeZeile = eingabe.readLine();
+        }
+        eingabe.close(); // Datei schlieï¿½en
 
-	}
+    }
 
-	/**
-	 * Der übergebene Musiktitel wird in der Playlist eine Position weiter
-	 * bewegt.
-	 * 
-	 * @param musiktitel
-	 */
-	public void bewegeTitelEinePositionWeiter(Musiktitel musiktitel) {
-		PlaylistElement temp = root;
-		PlaylistElement prev = root;
-		PlaylistElement middle = root;
-		PlaylistElement next = root;
-		if (root != null) {
-			if (middle.value != musiktitel) {
-				while (middle.value != musiktitel) {
-					prev = middle;
-					middle = prev.nextElement;
-					if (middle.nextElement != null) {
-						next = middle.nextElement;
-					} else {
-						next = null;
-					}
-				}
-				temp = middle;
-				prev.nextElement = next;
-				temp.nextElement = next.nextElement;
-				next.nextElement = temp;
-			}
-			else{
-				temp = root;
-				root = root.nextElement;
-				temp.nextElement = root.nextElement;
-				root.nextElement = temp;
-			}
-		}
-	}
+    /**
+     * Der Ã¼bergebene Musiktitel wird in der Playlist eine Position weiter
+     * bewegt.
+     *
+     * @param musiktitel
+     */
+    public void bewegeTitelEinePositionWeiter(Musiktitel musiktitel) {
+        PlaylistElement temp = root;
+        PlaylistElement prev = root;
+        PlaylistElement middle = root;
+        PlaylistElement next = root;
 
-	/**
-	 * Der übergebene Musiktitel wird in der Playlist eine Position zurück
-	 * bewegt.
-	 * 
-	 * @param musiktitel
-	 */
-	public void bewegeTitelEinePositionZurueck(Musiktitel musiktitel) {
+        // Im Prinzip muss man nur Referenzen vertauschen. Darauf achten , das beim kopieren Objektreferenzen ihre
+        // Descriptoren nicht verlieren, da sonst der Garbage-Collector aktiv wird. DafÃ¼r kann man eine TemporÃ¤re Variable
+        // des gleichen Typs benutzen um den Descriptor zwischen zu speichern
+        if (root != null) {
+            if (middle.value != musiktitel) {
+                //Schleife lÃ¤uft solange durch bis der Musiktitel welcher verschoben soll erreicht wird.
+                while (middle.value != musiktitel) {
+                    prev = middle;
+                    middle = prev.nextElement;
+                    if (middle.nextElement != null) {
+                        next = middle.nextElement;
+                    } else {
+                        next = null;
+                    }
+                }
+                temp = middle;
+                prev.nextElement = next;
+                temp.nextElement = next.nextElement;
+                next.nextElement = temp;
+            } else {
+                //Else behandelt wenn die oberen beiden Elemente der Liste(Queu) verschoben werden.
+                temp = root;
+                root = root.nextElement;
+                temp.nextElement = root.nextElement;
+                root.nextElement = temp;
+            }
+        }
+    }
 
-		PlaylistElement temp = root;
-		PlaylistElement prev = root;
-		PlaylistElement middle = root;
-		PlaylistElement next = root;
-		if (root != null) {
-			if (middle.nextElement.value != musiktitel) {
-				while (middle.nextElement.value != musiktitel) {
-					prev = middle;
-					middle = prev.nextElement;
-					if (middle.nextElement != null) {
-						next = middle.nextElement;
-					} else {
-						next = null;
-					}
-				}
-				System.out.println(prev.value);
-				System.out.println(middle.value);
-				System.out.println(next.value);
-				temp = middle;
-				prev.nextElement = next;
-				temp.nextElement = next.nextElement;
-				next.nextElement = temp;
-			} else {
-				temp = root;
-				root = root.nextElement;
-				temp.nextElement = root.nextElement;
-				root.nextElement = temp;
-			}
-		}
-	}
+    /**
+     * Der Ã¼bergebene Musiktitel wird in der Playlist eine Position zurÃ¼ck
+     * bewegt.
+     *
+     * @param musiktitel
+     */
+    public void bewegeTitelEinePositionZurueck(Musiktitel musiktitel) {
 
-	/**
-	 * Liefert den Namen dieser Playlist
-	 * 
-	 * @return Der Name dieser Playlist
-	 */
-	public String getName() {
-		return this.name;
-	}
+        PlaylistElement temp = root;
+        PlaylistElement prev = root;
+        PlaylistElement middle = root;
+        PlaylistElement next = root;
+        // Im Prinzip muss man nur Referenzen vertauschen. Darauf achten , das beim kopieren Objektreferenzen ihre
+        // Descriptoren nicht verlieren, da sonst der Garbage-Collector aktiv wird. DafÃ¼r kann man eine TemporÃ¤re Variable
+        // des gleichen Typs benutzen um den Descriptor zwischen zu speichern
+        if (root != null) {
+            if (middle.nextElement.value != musiktitel) {
+                //Schleife lÃ¤uft solange durch bis der Musiktitel welcher verschoben soll erreicht wird.
+                while (middle.nextElement.value != musiktitel) {
+                    prev = middle;
+                    middle = prev.nextElement;
+                    if (middle.nextElement != null) {
+                        next = middle.nextElement;
+                    } else {
+                        next = null;
+                    }
+                }
+                System.out.println(prev.value);
+                System.out.println(middle.value);
+                System.out.println(next.value);
+                temp = middle;
+                prev.nextElement = next;
+                temp.nextElement = next.nextElement;
+                next.nextElement = temp;
+            } else {
+                //Else behandelt wenn die oberen beiden Elemente der Liste(Queu) verschoben werden.
+                temp = root;
+                root = root.nextElement;
+                temp.nextElement = root.nextElement;
+                root.nextElement = temp;
+            }
+        }
+    }
 
-	/**
-	 * Verändert den Namen dieser Playlist
-	 * 
-	 * @param name
-	 *            Der neue Name dieser Playlist
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Liefert den Namen dieser Playlist
+     *
+     * @return Der Name dieser Playlist
+     */
+    public String getName() {
+        return this.name;
+    }
 
-	/**
-	 * Hängt eine andere Playlist an diese Playlist an.
-	 */
-	public void haengePlaylistAn(Playlist anderePlaylist) {
+    /**
+     * VerÃ¤ndert den Namen dieser Playlist
+     *
+     * @param name Der neue Name dieser Playlist
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-		PlaylistElement temp = root;
+    /**
+     * HÃ¤ngt eine andere Playlist an diese Playlist an.
+     */
+    public void haengePlaylistAn(Playlist anderePlaylist) {
 
-		if (temp == null) {
-			root = anderePlaylist.root;
-		} else {
-			while (temp.nextElement != null) {
-				temp = temp.nextElement;
-			}
-			temp.nextElement = anderePlaylist.root;
-			System.out.println(anderePlaylist.root.value);
-		}
-	}
+        PlaylistElement temp = root;
+
+        if (temp == null) {
+            root = anderePlaylist.root;
+        } else {
+            //Schleife durchlÃ¤uft solange bis das Ende erreicht ist.
+            // Notwendig, da wir an das Ende die neuen Elemente anfÃ¼gen mÃ¶chten
+            while (temp.nextElement != null) {
+                temp = temp.nextElement;
+            }
+            temp.nextElement = anderePlaylist.root;
+        }
+    }
 }
